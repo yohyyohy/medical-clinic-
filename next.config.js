c9523c123f9dd2918163ad10d48b544b5aac52a0
坂本 洋平 <sakamotoyouhei@MacBook-Pro-270.local>
2025-03-12 19:46:00 +0900

    Fix next-sitemap issue in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+const { withSitemap } = require('next-sitemap');
+
+module.exports = withSitemap({
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
+  siteUrl: process.env.SITE_URL || "https://www.sakamoto-clinic.com",
+  generateRobotsTxt: true,
+  exclude: ["/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://www.sakamoto-clinic.com/server-sitemap.xml",
+    ],
+  },
+  sitemapSize: 5000,
+  outDir: "public",
+});
